388c167e32c7416d5bd9a026034e1204202246a6
Paul 💩 <paul@mustard.lol>
2021-04-25 20:28:17 -0700

    Add 2015-07.

--- app/src/main/kotlin/kotlinadventofcode/Problem.kt
+++ app/src/main/kotlin/kotlinadventofcode/Problem.kt
@@ -1,6 +1,7 @@
 package kotlinadventofcode
 
 // DO NOT REMOVE ME. Just a lowly comment here trying to help with code generation.
+import kotlinadventofcode.`2015`.`2015-07`
 import kotlinadventofcode.`2015`.`2015-06`
 import kotlinadventofcode.`2015`.`2015-05`
 import kotlinadventofcode.`2015`.`2015-04`
@@ -24,7 +25,9 @@ enum class Problem(val day: Day, val part: Int): Day by day {
     `2015-05-1`(`2015-05`(), 1),
     `2015-05-2`(`2015-05`(), 2),
     `2015-06-1`(`2015-06`(), 1),
-    `2015-06-2`(`2015-06`(), 2);
+    `2015-06-2`(`2015-06`(), 2),
+    `2015-07-1`(`2015-07`(), 1),
+    `2015-07-2`(`2015-07`(), 2);
 
     fun run(): String {
         return if (part == 1) day.runPart1() else day.runPart2()
