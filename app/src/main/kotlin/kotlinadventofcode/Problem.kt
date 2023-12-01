7825d387fc78fd6f067d3b868e84594acda300ad
Paul 💩 <paul@mustard.lol>
2023-11-30 21:15:04 -0800

    Add 2023 day 1

--- app/src/main/kotlin/kotlinadventofcode/Problem.kt
+++ app/src/main/kotlin/kotlinadventofcode/Problem.kt
@@ -1,6 +1,7 @@
 package kotlinadventofcode
 
 // DO NOT REMOVE ME. Just a lowly comment here trying to help with code generation.
+import kotlinadventofcode.`2023`.`2023-01`
 import kotlinadventofcode.`2016`.`2016-01`
 import kotlinadventofcode.`2022`.`2022-18`
 import kotlinadventofcode.`2022`.`2022-17`
@@ -126,7 +127,9 @@ enum class Problem(val day: Day, val part: Int): Day by day {
     `2022-18-1`(`2022-18`(), 1),
     `2022-18-2`(`2022-18`(), 2),
     `2016-01-1`(`2016-01`(), 1),
-    `2016-01-2`(`2016-01`(), 2);
+    `2016-01-2`(`2016-01`(), 2),
+    `2023-01-1`(`2023-01`(), 1),
+    `2023-01-2`(`2023-01`(), 2);
 
     fun run(): String {
         return if (part == 1) day.runPart1() else day.runPart2()
